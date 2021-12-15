c9c33b1f879ed6ec4484ebf9b156cf9534b24007
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-12-15 14:38:56 -0300

    update

--- scriptlista.js
+++ scriptlista.js
@@ -1,67 +1,56 @@
+const description = document.getElementById("description");
+const ul = document.getElementById("itens");
+const Tasks = [];
 
-document.getElementById("botao").addEventListener("click",criarTarefa)
+const checkbox = '<input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">'
 
-var tarefas = [];
-    var lista = document.getElementById("itens");
-    var descricao = document.getElementById("descricao");
-    descricao.addEventListener("keypress",adicionarTarefa);
+document.getElementById("add").addEventListener("click",()=>{ //create task
 
+  Tasks.push(description.value);
+  showTasks()
+  description.value=" ";
+  description.focus()
+})
+
+function showTasks (){
+  ul.innerHTML = " ";
+  for(task of Tasks){
+    const li = document.createElement("li");
+    const item = document.createTextNode(task);
+
+    const done = document.createElement("input")
+    done.setAttribute('type', "checkbox")
+    li.appendChild(done)
+    
+    li.appendChild(item)
+    ul.appendChild(li)
+
+    let pos = description.value
+
+    const ok = document.createElement("button")
+    ok.textContent = "X";
+    li.appendChild(ok)
+
+    const atC = document.createAttribute("class")
+    atC.value = "btn btn-outline-danger ml-5 ok";
+    ok.setAttributeNode(atC)
+
+    var at = document.createAttribute("class"); //adicionando clss ao botão realizada
+    at.value = "form-check-input mt-2 ml-5";
+    done.setAttributeNode(at);
+    done.addEventListener("click",()=>{li.classList.toggle("riscar");})
+
+    // document.getElementsByClassName('at')[0].addEventListener("click",()=>{li.classList.toggle("riscar");})
 
     
-    function tamnhoTexto (){
-        return descricao.value.length;
-    }
-
-function criaritem() {
-    
-    if (tamnhoTexto() == 0) {
-        //excluir posição do array que não tem nada escrito
-      }
-    
-    var li = document.createElement("li");
-    li.textContent = descricao.value;
-    tarefas.push(descricao.value);
-    lista.appendChild(li); //foi criado o conteúdo de cada item e já foi anexado na lista.
-    var pos = tarefas.indexOf(descricao.value)
-    descricao.value="";
-    descricao.focus(); 
-
-    let bt1 = document.createElement("button"); //criando botão para riscar a atividade
-    bt1.textContent = " Realizada";
-    li.appendChild(bt1);
-    let at = document.createAttribute("class"); //adicionando clss ao botão realizada
-    at.value = "realizada";
-    bt1.setAttributeNode(at);                    
-
-    let bt2 = document.createElement("button");//criando botão para apagar atividade
-    bt2.textContent = " apagar";
-    li.appendChild(bt2);     
-    let atr = document.createAttribute("class"); //adicionando clss ao botão realizada
-    atr.value = "concluida";
-    bt2.setAttributeNode(atr);          
-    
-    bt1.addEventListener("click",riscar)
-    bt2.addEventListener("click",apagar)
-
-
-    function riscar() {
-                                    //o toogle pega a nossa lista e verefica se tem a class riscar, se tiver ela apaga
-    li.classList.toggle("riscar");  //Se não encontrar ela cria
-    console.log(tarefas)
-    }
-  
-    
-  function apagar (pos){
-    li.classList.add("apagar"); // o add pega o nossa lista e verifica se tem a class apagar, se não, ela cria a class
+    // ok.setAttribute("onclick",apagar(pos))
+    document.getElementsByClassName('ok').addEventListener('click', apagar(pos))
   }
 }
 
-function criarTarefa(){
-    if(tamnhoTexto() > 0 )
-    criaritem()
-}
 
-function adicionarTarefa(){
-    if (tamnhoTexto() > 0 && event.which == 13)
-    criaritem()
+function apagar(pos){
+  console.log(pos)
+  Tasks.splice(pos,1);
+  showTasks()
 }
